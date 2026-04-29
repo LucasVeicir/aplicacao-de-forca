56de31093f372c9e85e96b13c7fcadd245d8300d
Lucas Veicir <nikliklucas@gmail.com>
2026-04-29 08:51:34 -0300

    paredes e bola

--- ground.js
+++ ground.js
@@ -8,4 +8,14 @@ class Ground{
         this.h = h;
         World.add(world,this.body);
     }
+
+    show(){
+        let pos = this.body.position;
+        push();
+        rectMode(CENTER);
+        stroke(255);
+        fill(127);
+        rect(pos.x,pos.y,this.w,this.h);
+        pop()
+    }
 }
--- main.js
+++ main.js
@@ -3,13 +3,41 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 let engine;
 let world;
+let ground;
+let right;
+let left;
+let topWall;
+let ball;
 
 function setup(){
     createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    ground = new Ground(200,390,400,20);
+    right = new Ground(390,200,20,400);
+    left = new Ground(10,200,20,400);
+    topWall = new Ground(200,10,400,20);
+
+    let ballOptions = {
+        restitution:0.95
+    }
+    ball = Bodies.circle(200,100,20,ballOptions);
+    World.add(world,ball);
 }
 
 function draw(){
     background(51);
     Engine.update(engine);
 
+    ground.show();
+    right.show();
+    left.show();
+    topWall.show();
+
+    ellipse(ball.position.x,ball.position.y,20);
+}
+
+function hForce(){
+    Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});
 }
